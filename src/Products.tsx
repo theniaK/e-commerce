3f2049c73dc93bbae91457334c78bd284e348838
Thenia Koutrouli <theniakoutrouli@gmail.com>
2024-11-29 00:09:31 +0100

    add item dialogue

--- src/Products.tsx
+++ src/Products.tsx
@@ -4,7 +4,9 @@ import {
   Box,
   Button,
   Card,
-  CardActionArea,
+  Dialog,
+  DialogActions,
+  DialogContent,
   CardContent,
   CardMedia,
   makeStyles,
@@ -24,6 +26,9 @@ const useStyles = makeStyles(() => ({
     height: "300px",
     backgroundColor: "#F8F8F8",
   },
+  dialogue: {
+    backgroundColor: "#F8F8F8",
+  },
 }));
 
 export default function Products(): React.ReactElement {
@@ -31,17 +36,30 @@ export default function Products(): React.ReactElement {
   const items = products;
   const [data, setData] = useState<any[]>([]);
 
+  const [openDialog, setOpenDialog] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  function onCardClick(item: any) {
+    setSelectedItem(item);
+    setOpenDialog(true);
+  }
+
+  function handleCloseDialog() {
+    setOpenDialog(false);
+    setSelectedItem(null);
+  }
+
   useEffect(() => {
     setData(items);
   }, [products]);
 
-  const truncateText = (text: any, wordLimit: any) => {
+  function truncateText(text: any, wordLimit: any) {
     const words = text.split(" ");
     if (words.length > wordLimit) {
       return words.slice(0, wordLimit).join(" ") + "...";
     }
     return text;
-  };
+  }
 
   return (
     <div>
@@ -49,84 +67,86 @@ export default function Products(): React.ReactElement {
         data.length > 0 ? (
           data.map((item) => (
             <Card key={item.id} className={classes.card}>
-              <CardActionArea>
-                <CardMedia
-                  style={{
-                    borderRadius: "1px",
-                    paddingTop: "10px",
-                    marginLeft: "107px",
-                    width: "80px",
-                    height: "100px",
-                  }}
-                  component="img"
-                  height="140"
-                  image={item.Image}
-                />
-                <CardContent
-                  style={{ paddingBottom: "200px", paddingTop: "10px" }}
-                >
-                  <div>
-                    <Typography
-                      style={{
-                        fontSize: "12px",
-                        width: "35%",
-                        marginLeft: "85px",
-                        marginBottom: "10px",
-                        backgroundColor: "#FFC0CB",
-                      }}
-                      component="div"
-                    >
-                      {item.Category}
-                    </Typography>
-                  </div>
-                  <div>
-                    <Typography
-                      style={{
-                        fontSize: "20px",
-                        fontFamily: "Candara, sans-serif",
-                        fontWeight: "bold",
-                      }}
-                      component="div"
-                    >
-                      {item.Title}
-                    </Typography>
-                  </div>
-                  <div>
-                    <Typography
-                      style={{
-                        fontSize: "12px",
-                        overflow: "hidden",
-                      }}
-                    >
-                      {truncateText(item.Description, 18)}
-                    </Typography>
-                  </div>
-                  <Box>
-                    <Typography
-                      style={{
-                        fontFamily: "Candara, sans-serif",
-                        fontWeight: "bold",
-                        fontSize: "20px",
-                        position: "absolute",
-                        marginLeft: "65px",
-                        marginTop: "22px",
-                      }}
-                    >
-                      {item.Price}$
-                    </Typography>
-                    <Button
-                      style={{
-                        backgroundColor: "#d3c9ca",
-                        position: "absolute",
-                        marginLeft: "0",
-                        marginTop: "20px",
-                      }}
-                    >
-                      <ShoppingCartOutlinedIcon />
-                    </Button>
-                  </Box>
-                </CardContent>
-              </CardActionArea>
+              <CardMedia
+                style={{
+                  borderRadius: "1px",
+                  paddingTop: "10px",
+                  marginLeft: "107px",
+                  width: "80px",
+                  height: "100px",
+                }}
+                component="img"
+                height="140"
+                image={item.Image}
+              />
+              <CardContent
+                style={{ paddingBottom: "200px", paddingTop: "10px" }}
+              >
+                <div>
+                  <Typography
+                    style={{
+                      fontSize: "12px",
+                      width: "35%",
+                      marginLeft: "85px",
+                      marginBottom: "10px",
+                      backgroundColor: "#FFC0CB",
+                    }}
+                    component="div"
+                  >
+                    {item.Category}
+                  </Typography>
+                </div>
+                <div>
+                  <Typography
+                    style={{
+                      fontSize: "20px",
+                      fontFamily: "Candara, sans-serif",
+                      fontWeight: "bold",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => onCardClick(item)}
+                    component="div"
+                  >
+                    {item.Title}
+                  </Typography>
+                </div>
+                <div>
+                  <Typography
+                    style={{
+                      fontSize: "12px",
+                      overflow: "hidden",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => onCardClick(item)}
+                  >
+                    {truncateText(item.Description, 18)}
+                  </Typography>
+                </div>
+                <Box>
+                  <Typography
+                    style={{
+                      fontFamily: "Candara, sans-serif",
+                      fontWeight: "bold",
+                      fontSize: "20px",
+                      position: "absolute",
+                      marginLeft: "65px",
+                      marginTop: "22px",
+                    }}
+                  >
+                    {item.Price}$
+                  </Typography>
+                  <Button
+                    style={{
+                      backgroundColor: "#d3c9ca",
+                      position: "absolute",
+                      marginLeft: "0",
+                      marginTop: "20px",
+                    }}
+                  >
+                    <ShoppingCartOutlinedIcon />
+                  </Button>
+                </Box>
+              </CardContent>
             </Card>
           ))
         ) : (
@@ -145,6 +165,73 @@ export default function Products(): React.ReactElement {
           </p>
         </div>
       )}
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogContent className={classes.dialogue}>
+          {selectedItem && (
+            <div>
+              <img
+                src={selectedItem.Image}
+                style={{
+                  width: "200px",
+                  height: "250px",
+                  borderRadius: "4px",
+                  marginTop: "10px",
+                  marginLeft: "170px",
+                }}
+              />
+              <Typography
+                style={{
+                  fontSize: "25px",
+                  fontFamily: "Candara, sans-serif",
+                  fontWeight: "bold",
+                  marginTop: "30px",
+                  marginBottom: "30px",
+                }}
+              >
+                {selectedItem.Title}
+              </Typography>
+              <Typography
+                style={{
+                  fontSize: "14px",
+                  fontFamily: "Arial, sans-serif",
+                  marginBottom: "50px",
+                  alignItems: "center",
+                }}
+              >
+                {selectedItem.Description}
+              </Typography>
+              <Typography
+                style={{
+                  fontSize: "25px",
+                  fontFamily: "Candara, sans-serif",
+                  fontWeight: "bold",
+                  marginBottom: "10px",
+                }}
+              >
+                Price: {selectedItem.Price}$
+              </Typography>
+            </div>
+          )}
+        </DialogContent>
+        <DialogActions className={classes.dialogue}>
+          <Button
+            onClick={handleCloseDialog}
+            style={{
+              fontFamily: "Candara, sans-serif",
+              color: "black",
+              backgroundColor: "#FFC0CB",
+              width: "100px",
+              height: "30px",
+              borderRadius: "4px",
+              paddingTop: "5px",
+              marginBottom: "10px",
+              marginRight: "10px",
+            }}
+          >
+            Back
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
